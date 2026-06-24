ebb94ee4f39821f8130ef58ef4963265ad975c8d
minijb <1414309848@qq.com>
2026-06-24 19:34:41 +0800

    add claude startup

---
+++ sub/claude.sh
@@ -0,0 +1,4 @@
+npm install -g @anthropic-ai/claude-code
+curl -fsSL https://github.com/SaladDay/cc-switch-cli/releases/latest/download/install.sh | bash
+echo 'export PATH="/home/zhouhao/.local/bin:$PATH"' >> ~/.bashrc
+
